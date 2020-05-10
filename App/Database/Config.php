546f17faef5de0377d771b1ba4cd25fabfa5a3fb
SandraFW <55853937+SandraFW@users.noreply.github.com>
2020-05-10 22:01:29 +0200

    Update Config.php

--- App/Database/Config.php
+++ App/Database/Config.php
@@ -1,6 +1,6 @@
 <?php
-    define('DB_SERVER', "localhost");
-    define('DB_USER', "root");
-    define('DB_PASS', "");
-    define('DB_DATABASE', "clinic_project");
+    define("DB_SERVER", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_DATABASE", "Project");
  ?>
