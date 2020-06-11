1238d039a831dbe66ac7392a11255fcc0bffdb96
SandraFW <55853937+SandraFW@users.noreply.github.com>
2020-06-11 16:07:25 +0200

    Update PatientController.php

--- App/Controller/PatientController.php
+++ App/Controller/PatientController.php
@@ -38,8 +38,51 @@ public function edit() {
 
 	$this->model->editPatient($Fname,$Mname,$Lname,$Uname,$gender,$mobile,$email,$encrypted,$Day,$Month,$Year);
 }
+public function insertMed()
+{
+$Height=$_REQUEST['Height'];
+$Weight=$_REQUEST['Weight'];
 
-
+if(isset($_REQUEST['Radio']))
+{
+	$check=$_REQUEST['Radio'];
+	if($check=='Yes')
+	{
+	$allergies=$_REQUEST['allergies'];
+	}
+	else{
+		$allergies="None";
+	}
+}
+if(isset($_REQUEST['Radio2']))
+{
+	$check=$_REQUEST['Radio2'];
+	if($check=='Yes2')
+	{
+	$medications=$_REQUEST['medications'];
+	}
+	else{
+		$medications="None";
+	}
+}
+if(!empty($_REQUEST['Illnesses'])) {
+	if (is_array($_REQUEST['Illnesses']) || is_object($_POST['Illnesses']) )
+	 {
+	$Illnesses= implode(',', $_REQUEST['Illnesses']);
+	if(!empty($_REQUEST['Other'])) {
+		$value2=$_REQUEST['Illness'];
+		$Illnesses=$Illnesses.$value2;
+	}
+}
+}
+else
+{
+	if(!empty($_REQUEST['Other'])) {
+		$Illnesses=$_REQUEST['Illness'];
+}
+}
+$this->model->AddMedical($Height,$Weight,$allergies,$medications,$Illnesses);
+}
 }
 
 
